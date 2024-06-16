900402272609c3d7cf057cffba6ea048b47527aa
LisaMaPa <100318709+LisaMaPa@users.noreply.github.com>
2024-06-16 21:31:26 +0200

    updated commuter class and working on transportation
    To align every class for the simulate class.

--- Agent.py
+++ Agent.py
@@ -1,66 +1,69 @@
 import numpy as np
 from scipy import stats
-import mesa as ms 
 import matplotlib.pyplot as plt
 from Functions import truncated_normal_rvs
 
-
-#TODO:
-# Update
-# Memory
 
 class Commuter():
     '''
     Agent class for commuters
     '''
-    def __init__(self, unique_id, modes_of_transport = ['Ferry', 'Speedboat']):
+    def __init__(self, unique_id, modes_of_transport):
         '''
         Initialize the agent
         args: 
             unique_id: int, unique identifier for the agent
         '''
         self.unique_id = unique_id
-        self.state = np.random.choice(modes_of_transport)
-        self.pref_price, self.pref_crowd, self.pref_time = truncated_normal_rvs(0.0, 1.0, 0.5, 0.5/3, 3)
-        self.choice([0,1])
 
+        # Initial state for transportation
+        self.state = np.random.choice(modes_of_transport)
+
+        # Initialise preferences for price, density, and time
+        self.pref_price, self.pref_density, self.pref_time = truncated_normal_rvs(0.0, 1.0, 0.5, 0.5/3, 3)
+    
+        # Agent memory concerning price, density, time for each transportation mode
         self.memory = {}
         for mode in modes_of_transport:
-            print(mode)
-            self.memory[mode] = {'Price': 0, 'Density': 0, 'Time': 0}
-        print(self.memory)
+            self.memory[mode] = {'Price': [], 'Density': [], 'Time': []}
 
 
-    def choice(self, mode_of_transport):
+    def update_memory(self, mode):
+        self.memory[mode]['Price'].append(mode.price)
+        self.memory[mode]['Density'].append(mode.density)
+        self.memory[mode]['Time'].append(mode.time)
+
+
+    def choice_of_transportation(self, mode_of_transport):
+        ''' Choose the best transportation mode based on utility. '''
+    
         mode_utility = {}
 
+        # For each mode of transportation calculate the utility
         for mode in mode_of_transport:
+            self.update_memory(mode) # Update memory before making choice
             mode_utility[mode] = self.utility(mode)
 
+        # Define best mode of transport based on highest utility score
         best_option = max(mode_utility, key=mode_utility.get)
 
         return best_option
 
-
-    def utility(self, mode):
-        self.memory[mode]['Price'].append(mode.price)
-        self.memory[mode]['Density'].append(mode.density)
-        self.memory[mode]['Time'].append(mode.time)
         
-        if len(self.memory[mode]['Price']) > 5:
-            average_price_memory = np.mean(self.memory['Price'][-5:])
-            average_density_memory = np.mean(self.memory['Density'][-5:])
-            average_time_memory = np.mean(self.memory['Time'][-5:])
+    def utility(self, mode):
+        ''' Calculate utility for a given mode based on historical data. '''
+        if len(self.memory[mode]['Price']) > 0:
+            average_price_memory = np.mean(self.memory[mode]['Price'][-5:])
+            average_density_memory = np.mean(self.memory[mode]['Density'][-5:])
+            average_time_memory = np.mean(self.memory[mode]['Time'][-5:])
         else:
-            average_price_memory = np.mean(self.memory['Price'])
-            average_density_memory = np.mean(self.memory['Density'])
-            average_time_memory = np.mean(self.memory['Time'])
+            average_density_memory = average_density_memory = average_time_memory = 0
 
-        return self.pref_price*average_price_memory +self.pref_crowd*average_density_memory + self.pref_time*average_time_memory
+        return (- self.pref_price * average_price_memory 
+                - self.pref_density * average_density_memory 
+                - self.pref_time * average_time_memory)
 
 
-
-agent = Commuter(0)
 
 
 
