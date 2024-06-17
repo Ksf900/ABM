87e1973b58af0897340bff1fc0528a58e0fb435b
LisaMaPa <100318709+LisaMaPa@users.noreply.github.com>
2024-06-17 21:14:12 +0200

    update

--- Agent.py
+++ Agent.py
@@ -6,46 +6,67 @@ from Functions import truncated_normal_rvs
 
 class Commuter():
     '''
-    Agent class for commuters
+    Represents a commuter in a transportation simulation.
+    
+    Attributes:
+        commuter_id (int): A unique identifier for the commuter.
+        location (str): Current location of the commuter.
+        modes_of_transportation (list): Available transportation modes.
+        preferences (dict): Commuter's preferences for price, density, and time.
+        memory (dict): Historical data of experiences with each transportation mode.
     '''
-    def __init__(self, unique_id, modes_of_transport):
-        '''
-        Initialize the agent
-        args: 
-            unique_id: int, unique identifier for the agent
-        '''
-        self.unique_id = unique_id
-
-        # Initial state for transportation
-        self.state = np.random.choice(modes_of_transport)
+    def __init__(self, commuter_id, start_location, modes_of_transport):
+        self.commuter_id = commuter_id
+        self.location = start_location
+        self.modes_of_transportation = modes_of_transport
 
         # Initialise preferences for price, density, and time
         self.pref_price, self.pref_density, self.pref_time = truncated_normal_rvs(0.0, 1.0, 0.5, 0.5/3, 3)
     
         # Agent memory concerning price, density, time for each transportation mode
         self.memory = {}
-        for mode in modes_of_transport:
+        for mode in self.modes_of_transport:
+            # Initialise a memory for each mode 
             self.memory[mode] = {'Price': [], 'Density': [], 'Time': []}
-
-
-    def update_memory(self, mode):
-        self.memory[mode]['Price'].append(mode.price)
-        self.memory[mode]['Density'].append(mode.density)
-        self.memory[mode]['Time'].append(mode.time)
-
-
-    def choice_of_transportation(self, mode_of_transport):
-        ''' Choose the best transportation mode based on utility. '''
+ 
     
-        mode_utility = {}
+    def get_available_modes(self):
+        """Returns a list of available transportation modes from the current location."""
+        available_modes = []
+        for mode in self.modes_of_transportation:
+            if mode.start_location == self.location:
+                available_modes.append(mode)
+        return available_modes
 
+
+    def update_location(self, chosen_mode):
+        """Updates the commuter's chosen_modelocation based on the end location of the chosen transportation mode."""
+        self.location = chosen_mode.end_location
+
+    def update_memory(self, chosen_mode):
+        """Updates memory for the chosen transportation mode with the latest trip data."""
+        self.memory[chosen_mode]['Price'].append(chosen_mode.price)
+        self.memory[chosen_mode]['Density'].append(chosen_mode.density)
+        self.memory[chosen_mode]['Time'].append(chosen_mode.time)
+
+
+    def choose_transportation(self):
+        ''' Choose the best transportation mode based on utility, and only on available modes. '''
+        
+        # First, define the available modes
+        available_modes = self.get_available_modes()
+
+        mode_utility = {}
+        
         # For each mode of transportation calculate the utility
-        for mode in mode_of_transport:
-            self.update_memory(mode) # Update memory before making choice
+        for mode in available_modes:
             mode_utility[mode] = self.utility(mode)
 
         # Define best mode of transport based on highest utility score
         best_option = max(mode_utility, key=mode_utility.get)
+        
+        # Update location of the commuter (this info is stored in the transport mode)
+        self.update_location(best_option)
 
         return best_option
 
@@ -57,29 +78,11 @@ class Commuter():
             average_density_memory = np.mean(self.memory[mode]['Density'][-5:])
             average_time_memory = np.mean(self.memory[mode]['Time'][-5:])
         else:
-            average_density_memory = average_density_memory = average_time_memory = 0
+            average_density_memory = average_price_memory = average_time_memory = 1
 
         return (- self.pref_price * average_price_memory 
                 - self.pref_density * average_density_memory 
                 - self.pref_time * average_time_memory)
-
-
-
-
-
-
-
-"""
-This part is for running the model. The argparse part asks the user to give a command-line argument for the number of agents.
-"""
-# if __name__ == "__main__":
-#     parser = argparse.ArgumentParser(description="Run an agent-based model simulation.")
-#     parser.add_argument('--num_agents', type=int, default=1000, help='Number of agents in the simulation')
-    
-#     args = parser.parse_args()
-    
-#     main(args.num_agents)
-        
 
 
 
--- Simulate.py
+++ Simulate.py
@@ -3,3 +3,48 @@ from Functions import
 from Agent import
 from Policy import
 
+# Steps for simulation should be:
+# 1. initialise commuters
+
+# 2. choose location for a commuter
+
+# 3. collect amount of commuters taking a certain mode of transport
+
+# 4. update the conditions of the transportation modes (after each iteration/day)
+
+# 5. per commuter that used e.g. Ferry_AB it should update the memory of that specific ferry 
+# after each iteration and after updating the conditions of the transport because the transport class 
+# contains functions for calculating price, density, and time etc(this is quite difficult i think)
+
+# 6. start at 2.
+
+
+
+class Simulation:
+    '''
+    Transport modes should include a list (btw you should initialise ferry outside of simulation class before running:
+      ferry_AB = Ferry(A, B, amount_of_commuters, price, time)
+    such as transport_modes = [ferry_AB, ferry_BA, speedboat_AB, speedboat_BA]
+    And commuters is the amount of commuters e.g. 100 or something
+    '''
+    def __init__(self, commuters, transport_modes):
+        self.commuters = commuters
+        self.transport_modes = transport_modes
+
+    def run_simulation(self, num_days):
+        for day in range(num_days):
+            self.simulate_day()
+
+    def simulate_day(self):
+        # Simulate a day of commuting
+        for commuter in self.commuters:
+            
+            # Choose best mode
+            best_mode = commuter.choose_transportation()
+
+            # 
+        
+
+            
+        
+        
