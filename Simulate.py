cb2e9e5e0f68e5eaee12f5038ca2ceb7d1c7f6f4
AA-ron2 <125910039+AA-ron2@users.noreply.github.com>
2024-06-19 11:02:35 +0200

    v2

--- Simulate.py
+++ Simulate.py
@@ -1,10 +1,11 @@
-from Transport import Ferry, Speedboat
+from transport import Ferry, Speedboat
 from Functions import truncated_normal_rvs
 from Agent import Commuter
 
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
-
+from mesa.datacollection import DataCollector
 
 # Steps for simulation should be something like this:
 # 1. initialise commuters
@@ -124,5 +125,134 @@ simulation = Simulation(
     speedboat_base_price=5, 
     speedboat_base_time=20
 )
+
 simulation.run()
 simulation.plot_results()
+
+
+
+
+################################################################################
+class Simulation:
+    def __init__(self, num_commuters, num_days, ferry_capacity, ferry_base_price, ferry_base_time,
+                 speedboat_base_price, speedboat_base_time):
+        self.num_commuters = num_commuters
+        self.num_days = num_days
+        self.ferry_AB = Ferry("Island_A", "Island_B", ferry_capacity, ferry_base_price, ferry_base_time)
+        self.speedboat_AB = Speedboat("Island_A", "Island_B", 200, speedboat_base_price, speedboat_base_time)
+        self.ferry_BA = Ferry("Island_B", "Island_A", ferry_capacity, ferry_base_price, ferry_base_time)
+        self.speedboat_BA = Speedboat("Island_B", "Island_A", 200, speedboat_base_price, speedboat_base_time)
+        self.commuters = [Commuter(i, "Island_A", [self.ferry_AB, self.speedboat_AB, self.ferry_BA, self.speedboat_BA]) for i in range(num_commuters)]
+
+        self.datacollector = DataCollector(
+            model_reporters={
+                'day': lambda m: m.current_day,
+                'ferry_AB_users': lambda m: m.ferry_AB.number_of_mode_users,
+                'speedboat_AB_users': lambda m: m.speedboat_AB.number_of_mode_users,
+                'ferry_BA_users': lambda m: m.ferry_BA.number_of_mode_users,
+                'speedboat_BA_users': lambda m: m.speedboat_BA.number_of_mode_users,
+                'ferry_AB_density': lambda m: m.ferry_AB.density,
+                'speedboat_AB_density': lambda m: m.speedboat_AB.density,
+                'ferry_BA_density': lambda m: m.ferry_BA.density,
+                'speedboat_BA_density': lambda m: m.speedboat_BA.density,
+                'ferry_AB_time': lambda m: m.ferry_AB.time,
+                'speedboat_AB_time': lambda m: m.speedboat_AB.time,
+                'ferry_BA_time': lambda m: m.ferry_BA.time,
+                'speedboat_BA_time': lambda m: m.speedboat_BA.time
+            }
+        )
+        self.current_day = 0
+
+    def step(self):
+        daily_choices = {mode: 0 for mode in [self.ferry_AB, self.speedboat_AB, self.ferry_BA, self.speedboat_BA]}
+        commuter_choices = {}
+
+        for commuter in self.commuters:
+            chosen_mode = commuter.choose_transportation()
+            daily_choices[chosen_mode] += 1
+            commuter_choices[commuter] = chosen_mode
+
+        for mode, num_users in daily_choices.items():
+            mode.update_conditions(num_users)
+
+        for commuter, chosen_mode in commuter_choices.items():
+            commuter.update_memory(chosen_mode)
+
+        self.datacollector.collect(self)
+        self.current_day += 1
+
+    def run(self):
+        for _ in range(self.num_days):
+            self.step()
+        self.export_data()
+
+    def export_data(self, filename='simulation_data.csv'):
+        df = self.datacollector.get_model_vars_dataframe()
+        df.to_csv(filename)
+        print(f"Data exported to {filename}")
+
+    def plot_results(self):
+        df = self.datacollector.get_model_vars_dataframe()
+
+        plt.figure(figsize=(12, 10))
+
+        plt.subplot(2, 1, 1)
+        plt.plot(df.index, df['ferry_AB_users'], label='Ferry AB Users')
+        plt.plot(df.index, df['speedboat_AB_users'], label='Speedboat AB Users')
+        plt.plot(df.index, df['ferry_BA_users'], label='Ferry BA Users')
+        plt.plot(df.index, df['speedboat_BA_users'], label='Speedboat BA Users')
+        plt.xlabel('Day')
+        plt.ylabel('Number of Users')
+        plt.legend()
+        plt.title('Number of Users per Transportation Mode')
+
+        plt.subplot(2, 1, 2)
+        plt.plot(df.index, df['ferry_AB_time'], label='Ferry AB Time')
+        plt.plot(df.index, df['speedboat_AB_time'], label='Speedboat AB Time')
+        plt.plot(df.index, df['ferry_BA_time'], label='Ferry BA Time')
+        plt.plot(df.index, df['speedboat_BA_time'], label='Speedboat BA Time')
+        plt.xlabel('Day')
+        plt.ylabel('Travel Time')
+        plt.legend()
+        plt.title('Travel Time per Transportation Mode')
+
+        plt.tight_layout()
+        plt.show()
+        
+    def plot_specific_results(self, metrics):
+        data = self.datacollector.get_model_vars_dataframe()
+
+        plt.figure(figsize=(12, 10))
+        
+        for metric in metrics:
+            plt.plot(data.index, data[metric], label=metric.replace('_', ' ').title())
+        
+        plt.xlabel('Day')
+        plt.ylabel('Value')
+        plt.legend()
+        plt.title('Specific Metrics over Time')
+        plt.tight_layout()
+        plt.show()
+
+# Example usage
+simulation = Simulation(
+    num_commuters=200,
+    num_days=30,
+    ferry_capacity=200,
+    ferry_base_price=5,
+    ferry_base_time=40,
+    speedboat_base_price=5,
+    speedboat_base_time=20
+)
+
+simulation.run()
+simulation.plot_results()
+
+data = pd.read_csv("simulation_data.csv")
+data.plot()
+
+# Plot specific results
+metrics_to_plot = [
+    'ferry_BA_density', 'speedboat_BA_density'
+]
+simulation.plot_specific_results(metrics_to_plot)
